665368a931879e24e2f88bfd286107b6bfec86c3
abdullahyaqoob391 <140446519+abdullahyaqoob391@users.noreply.github.com>
2023-07-25 14:31:24 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,30 @@
+var myName = 'Hey Abdullah,';
+var message:string = `I have started learning TYPESCRIPT!`;
+console.log(myName, message);
+
+var a: number = 35;
+var b: number = 74;
+var c: number = 73;
+var d: number = 10;
+var e: number = 10;
+var f: number = 14;
+var g: number = 23;
+var h: number = 12;
+var i: number = 76;
+var j: number = 69;
+
+var adding:number = (a+b+c+d+e+f+g+h+i+j);
+console.log("By Adding Numbers");
+console.log(adding);
+
+var multiplying:number = (a*b*c*d*e*f*g*h*i*j);
+console.log("By Multiplying Numbers")
+console.log(multiplying);
+
+var subtracting:number = (a-b-c-d-e-f-g-h-i-j);
+console.log("By Subtracting Numbers");
+console.log(subtracting);
+
+var dividing:number = (d/e);
+console.log("By Dividing Numbers");
+console.log(dividing);
